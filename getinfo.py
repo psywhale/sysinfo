8c1ac6623483d259117d1a2be33ad46d5099e56e
psywhale <psycho.whale@gmail.com>
2017-03-30 16:23:02 -0500

    wip generating MD docs now! Need a way to not overwrite MD  that already exists..

--- getinfo.py
+++ getinfo.py
@@ -201,6 +201,4 @@ if __name__ == '__main__':
                                              " by hand.",
                         action="store_true")
     args = parser.parse_args()
-
-
     main()
